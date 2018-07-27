09a174afe0d275b0712b5389ec6d074c73155107
nicholaschun <nicholaschunrayne@gmail.com>
2018-07-27 23:16:47 +0000

    Basic react starter

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../assets/styles/App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div class="full-container">
+        <h2>Welcome to react starter crafted with &hearts;</h2>
+      </div>
+    )
+  }
+}
+
+export default App;
